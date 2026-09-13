1a0b89062417f10005d86d1b90d8dde80b71df2c
ademirbgol <ademir.gonzaga.oliveira@escola.pr.gov.br>
2026-09-13 00:02:55 +0000

    app

--- app.py
+++ app.py
@@ -4,46 +4,45 @@ import os
 
 app = Flask(__name__)
 
-# Pasta onde os arquivos enviados serão salvos
+
 UPLOAD_FOLDER = "uploads"
 
-# Cria a pasta automaticamente caso ela não exista
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-# Página principal
+
 @app.route("/")
 def inicio():
     return render_template("index.html")
 
 
-# Recebe o PDF enviado pelo formulário
+
 @app.route("/upload", methods=["POST"])
 def upload():
 
-    # Verifica se o formulário enviou um arquivo
+   
     if "arquivo" not in request.files:
         return "Nenhum arquivo foi enviado."
 
     arquivo = request.files["arquivo"]
 
-    # Verifica se o usuário escolheu um arquivo
+    
     if arquivo.filename == "":
         return "Nenhum arquivo foi selecionado."
 
-    # Aceita somente PDF
+   
     if not arquivo.filename.lower().endswith(".pdf"):
         return "Erro: envie somente arquivos PDF."
 
-    # Caminho onde o arquivo será salvo
+   
     caminho = os.path.join(
         app.config["UPLOAD_FOLDER"],
         arquivo.filename
     )
 
-    # Salva o arquivo
+    
     arquivo.save(caminho)
 
     return f"""
@@ -73,7 +72,7 @@ def upload():
     """
 
 
-# Inicia o servidor
+
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
